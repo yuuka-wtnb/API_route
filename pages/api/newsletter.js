25b65abb65ce6f6bbea9ce5aa580cc0723ca0375
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-07-30 00:08:06 +0900

    Adding Error Handling

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,20 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://dbUser:dbUserpass@cluster0.d4gvdkd.mongodb.net/events?retryWrites=true&w=majority"
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  //inserting one document
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -10,17 +25,30 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://dbUser:dbUserpass@cluster0.d4gvdkd.mongodb.net/events?retryWrites=true&w=majority"
-    );
-    //access to the database
-    const db = client.db();
+    let client;
 
-    //inserting one document
-    await db.collection("newsletter").insertOne({ email: userEmail });
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed!" });
+      return;
+    }
+
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: "Inserting data failed!" });
+      return;
+    }
+
+    // //access to the database
+    // const db = client.db();
+
+    // //inserting one document
+    // await db.collection("newsletter").insertOne({ email: userEmail });
 
     //Disconnect
-    client.close();
 
     res.status(201).json({ message: "Signed up!" });
   }
