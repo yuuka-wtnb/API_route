5e8fbdd8bc7b11a0b549783ecdc642c269450f50
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-07-29 15:00:09 +0900

    Running MongoDB Queries From Inside API Routes

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,15 +1,29 @@
-function handler(req, res) {
-    if (req.method === "POST") {
-      const userEmail = req.body.email;
-  
-      // ここでデータベースにユーザーのメールアドレスを保存するなどの処理を実行します。
-      if (!userEmail || !userEmail.includes("@")) {
-        res.status(422).json({ message: "Invalid email address" });
-        return;
-      }
-      console.log(userEmail);
-      res.status(201).json({ message: 'Signed up!' });
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const userEmail = req.body.email;
+
+    // ここでデータベースにユーザーのメールアドレスを保存するなどの処理を実行します。
+    if (!userEmail || !userEmail.includes("@")) {
+      res.status(422).json({ message: "Invalid email address" });
+      return;
     }
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://dbUser:dbUserpass@cluster0.d4gvdkd.mongodb.net/?retryWrites=true&w=majority"
+    );
+    //access to the database
+    const db = client.db();
+
+    //inserting one document
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    //Disconnect
+    client.close();
+
+    res.status(201).json({ message: "Signed up!" });
   }
-  
-  export default handler;
+}
+
+export default handler;
