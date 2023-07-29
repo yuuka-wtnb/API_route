1a4803e221a175785d49545f8b099d79e520521f
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-07-30 07:03:03 +0900

    Automatically Removing Notifications on comment page

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,49 +1,86 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from "react";
 
-import CommentList from './comment-list';
-import NewComment from './new-comment';
-import classes from './comments.module.css';
+import CommentList from "./comment-list";
+import NewComment from "./new-comment";
+import classes from "./comments.module.css";
+import NotificationContext from "../../store/notification-context";
+
 function Comments(props) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const notificationCtx = useContext(NotificationContext);
 
   useEffect(() => {
     if (showComments) {
-      fetch('/api/comments/' + eventId)
+      fetch("/api/comments/" + eventId)
         .then((response) => response.json())
         .then((data) => {
           setComments(data.comments);
         });
     }
-  }, [showComments]);
+  }, [showComments, eventId]);
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
-
   }
 
   function addCommentHandler(commentData) {
-    fetch('/api/comments/' + eventId, {
-      method: 'POST',
+    notificationCtx.showNotification({
+      title: "Submitting comment...",
+      message: "Your comment is being submitted.",
+      status: "pending",
+    });
+
+    fetch("/api/comments/" + eventId, {
+      method: "POST",
       body: JSON.stringify(commentData),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        response.json().then((data) => {
+          throw new Error(data.message || "Something went wrong!");
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: "Success!",
+          message: "Your comment has been submitted successfully!",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: "Error!",
+          message: error.message || "Something went wrong!",
+          status: "error",
+        });
+      })
+      .finally(() => {
+        notificationCtx.showNotification({
+          title: "Loading...",
+          message: "Loading comments...",
+          status: "pending",
+        });
+      });
   }
 
   return (
     <section className={classes.comments}>
       <button onClick={toggleCommentsHandler}>
-        {showComments ? 'Hide' : 'Show'} Comments
+        {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
       {showComments && <CommentList items={comments} />}
     </section>
   );
 }
+
 export default Comments;
